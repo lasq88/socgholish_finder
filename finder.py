d7b210450cd884d950db7e5a09358bf604402797
Josh <joshnck@gmail.com>
2022-11-05 00:37:09 -0400

    modified code to accept csv!

--- finder.py
+++ finder.py
@@ -4,6 +4,8 @@ import re
 import requests
 import argparse
 import base64
+import csv
+import sys
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
@@ -16,28 +18,50 @@ indicators = [
 
 def GetWebsite(url, headers):
     try:
-        r = requests.get(url, headers=headers)
+        if (("https://" in url) | ("http://" in url)):
+            r = requests.get(url, headers=headers)
+        else:
+            if(url.startswith('//')):
+                url = url[2:]
+            elif(url.startswith('/')):
+                url=url[1:]
+            try:
+                url1 = "https://"+url
+                print("trying: "+url1)
+                r = requests.get(url1, headers=headers)
+            except:
+                print("trying http://"+url)
+                url2 = "http://"+url
+                r = requests.get(url2, headers=headers)
     except:
+        e = sys.exc_info()[0]
+        print(e)
         print("Cannot connect to {}".format(url))
-        exit()
+        r = ""
     return r 
 
 def ParseWebsite(url, ua):
-    r = GetWebsite(url, headers={'User-Agent': ua})
-    soup = BeautifulSoup(r.content, 'html.parser')
     scripts = []
-    for s in soup.findAll('script'):
-        src = s.get('src')
-        if src is None:
-            scripts.append((url,s.string))
-        else:
-            src_url = urljoin(url,src)
-            src_text = GetWebsite(src_url, headers={'User-Agent': ua})
-            if type(src_text.content) == bytes:
-                scripts.append((src_url,src_text.content.decode("UTF-8")))
+    r = GetWebsite(url, headers={'User-Agent': ua})
+    if(r == ""):
+        return scripts
+    else:
+        soup = BeautifulSoup(r.content, 'html.parser')
+        for s in soup.findAll('script'):
+            src = s.get('src')
+            if src is None:
+                scripts.append((url,s.string))
             else:
-                scripts.append((src_url,str(src_text.content)))
-    return scripts
+                src_url = urljoin(url,src)
+                src_text = GetWebsite(src_url, headers={'User-Agent': ua})
+                try:
+                    if type(src_text.content) == bytes:
+                        scripts.append((src_url,src_text.content.decode("UTF-8")))
+                    else:
+                        scripts.append((src_url,str(src_text.content)))
+                except:
+                    scripts = []
+        return scripts
 
 def FindSocGholish(scripts):
     potential_sg = []
@@ -73,22 +97,10 @@ def Stage2Url(script):
             return d
 
     return None
-1
 
-
-def main():
-    parser = argparse.ArgumentParser(description='SocGholish finder')
-    parser.add_argument("url", type=str, help="URL to check")
-    parser.add_argument("-ua", "--user-agent",type=str, help="Specify User-Agent to use with the request")
-    args = parser.parse_args()
-
-    if args.user_agent is None:
-        ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'
-    else:
-        ua = args.user_agent
-
-    print("Scanning website {} in progress...".format(args.url))
-    scripts = ParseWebsite(args.url, ua)
+def scan(url,ua):
+    print("Scanning website {} in progress...".format(url))
+    scripts = ParseWebsite(url, ua)
     sg = FindSocGholish(scripts)
     if sg != []:
         stage2 = []
@@ -97,7 +109,7 @@ def main():
             print("Potential injection script (matched {:d} out of {:d} indicators):".format(e[1],len(indicators)))
             print(e[0][1])
             print("")
-            stage2.append(urljoin(args.url,Stage2Url(e[0])))
+            stage2.append(urljoin(url,Stage2Url(e[0])))
         print("Trying to extract stage 2 urls...")
         print("")
         print("Potential Stage 2 URLs:")
@@ -105,13 +117,44 @@ def main():
             if "report" in u:
                 print(u)
             else:
-                response = GetWebsite(urljoin(args.url,u),headers={'Host': args.url.split('/')[2], 'User-Agent': ua, 'referer': args.url})
+                response = GetWebsite(urljoin(url,u),headers={'Host': url.split('/')[2], 'User-Agent': ua, 'referer': url})
                 s2url = Stage2Url(response.content)
                 if s2url is not None:
                     print(s2url)
                 
     else:
         print("Couldn't find any SocGholish payload :(")
+
+def main():
+    parser = argparse.ArgumentParser(description='SocGholish finder')
+    parser.add_argument("-url", type=str, help="URL to check")
+    parser.add_argument("-ua", "--user-agent",type=str, help="Specify User-Agent to use with the request")
+    parser.add_argument("-f", "--filename", type=str, help="csv of domains to check")
+    args = parser.parse_args()
+
+
+    if args.user_agent is None:
+        ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'
+    else:
+        ua = args.user_agent
+
+    if(args.filename):
+        print("Scanning file: "+ args.filename)
+        with open(args.filename, 'r') as csvFile:
+            raw_file = csv.reader(csvFile)
+            for row in raw_file:
+                url = str(row[0])
+                scan(url,ua)
+
+    if( not args.filename and args.url):
+        print("Scanning url " + args.url)
+        scan(args.url,ua)
+
+
+    if( (args.filename == "") & (args.url == "")):
+        print("Must input a URL or CSV file of domains")
+        exit()
+
 
 if __name__ == "__main__":
     main()
